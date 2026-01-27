1de18b3d605572b5cad9a49fdb239e4ae43979e1
soyesh-1 <soyeshxrestha@gmail.com>
2026-01-27 15:43:20 +0545

    Allow auth cookie across dev ports

--- ShoeCulture backend/src/utils/cookies.js
+++ ShoeCulture backend/src/utils/cookies.js
@@ -3,7 +3,7 @@ const { env } = require("../config/env");
 const getAuthCookieOptions = () => ({
   httpOnly: true,
   secure: env.nodeEnv === "production",
-  sameSite: "lax",
+  sameSite: env.nodeEnv === "production" ? "none" : "none",
   maxAge: 2 * 60 * 60 * 1000,
 });
 
